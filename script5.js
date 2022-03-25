c87d0d90eb50641b7bb0b6d51d178d00c48337c8
KRISHNA MISHRA <98013228+krishnamishrakk@users.noreply.github.com>
2022-03-25 18:23:20 +0530

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,62 @@
+// // fruits = ['apple', 'banana', 'orange', 'kiwi']
+
+// // fruits.pop();
+// // console.log(fruits)
+// // console.log(fruits.pop())
+// // console.log(fruits)
+// // console.log(fruits.push('strawberry'))
+// // console.log(fruits.push('melon'))
+// // console.log(fruits)
+// arr=[1,2,3,4,5,6,7,8,9];
+// console.log(arr);
+// let result=(arr.push(7,1,2,2,));
+// console.log(result);
+// function f1(){
+//     console.log('normal')
+// }
+// var f2=function (){
+//     console.log('anonymous function')
+// }
+// f2()
+// f1()
+//  var arr=[1,2,'hi', "bye",[4,6,5],2,"krishna",1.2]
+// //  console.log(arr[6])
+// // console.log(arr[4].length)
+// // console.log(arr.pop());
+// // console.log(arr)
+// // console.log(arr.push("kk"));
+// //  console.log(arr.push(222));
+// //  console.log(arr.length);
+// //  console.log(arr);
+// //  console.log(arr.pop());
+// //  console.log(arr);
+// console.log(arr.shift());
+// console.log(arr);
+// console.log(arr.push("KRISHNA"));
+// console.log(arr[6][5]);
+// console.log(arr);
+// console.log(arr.unshift('KK'));
+// console.log(arr);
+// console.log(arr.push(6));
+// console.log(arr);
+// console.log(arr.pop());
+// console.log(arr);
+// console.log(arr.shift());
+// console.log(arr);
+// console.log(arr.unshift(9));
+// console.log(arr);
+// console.log(arr.indexOf(1.2));
+// console.log(arr);
+// console.log(arr.slice(6));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(2,-2));
+// console.log(arr.includes('KK'));
+// console.log(arr.includes(1.2));
+// console.log(arr.length);
+// console.log(arr.lastIndexOf(2));
+
+ 
+ 
+
+
+    
